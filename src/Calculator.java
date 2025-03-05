3587a11d6d199709d281d8f856965f36242bb2dd
Andrii Sysoiev <sisoev.a@outlook.com>
2025-03-05 22:00:08 +0100

    GCD function tested and added

--- src/Calculator.java
+++ src/Calculator.java
@@ -23,4 +23,13 @@ public class Calculator {
         }
         return result;
     }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
