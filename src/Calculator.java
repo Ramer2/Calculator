bf1a5d3902665920086caab70040c6b3aab5ca64
Andrii Sysoiev <sisoev.a@outlook.com>
2025-03-05 21:58:01 +0100

    Power function tested and added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,10 @@
 public class Calculator {
+    public static long pow(long a, long b) {
+        long result = 1;
+        while (b > 0) {
+            result *= a;
+            b--;
+        }
+        return result;
+    }
 }
